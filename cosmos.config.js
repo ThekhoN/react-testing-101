c5cff11bef14481780184635cd7091c44d4ff6b3
Ngaosathe <662318@MACPC271482.local>
2018-06-29 15:54:27 +0530

    correct cosmos.proxies path in cosmos.config - thanks@skidding

--- cosmos.config.js
+++ cosmos.config.js
@@ -1,7 +1,6 @@
 module.exports = {
-  containerQuerySelector: '#root',
-  webpackConfigPath: 'react-scripts/config/webpack.config.dev',
-  publicPath: 'public',
-  // Optional: Create this file when you begin adding proxies
-  proxiesPath: 'src/cosmos.proxies'
+  containerQuerySelector: "#root",
+  webpackConfigPath: "react-scripts/config/webpack.config.dev",
+  publicPath: "public",
+  proxiesPath: "./cosmos.proxies"
 };
